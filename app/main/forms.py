14199f4289a124586e00e9feb1ccfcf410413ee7
ali <a.abrishami110@gmail.com>
2018-01-21 18:08:45 +0330

    image

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField , FileField
 from wtforms.validators import DataRequired, Length
 
 
 class AddPostForm(Form):
     title = StringField('Title', validators=[DataRequired()])
     post = TextAreaField('Text', validators=[DataRequired(), Length(min=10, max=158)])
-    image = StringField('Image', validators=[Length(min=6, max=512)])
+    image = FileField('Image', validators=[])
     link = StringField('Link', validators=[DataRequired()])
     tags = StringField('Tags')
 
