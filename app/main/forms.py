2ab44521f5cf5003a9cd3bda773293ef5c4fc6c2
amirabbas majd <amirabbasmajd@yahoo.com>
2018-01-25 17:24:33 +0330

    first final version

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
@@ -9,6 +9,15 @@ class AddPostForm(Form):
     post = TextAreaField('Text', validators=[DataRequired(), Length(min=10, max=158)])
     image = FileField('Image', validators=[
         FileRequired(),
+        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+    ])
+    link = StringField('Link', validators=[DataRequired()])
+    tags = StringField('Tags')
+
+class EditPostForm(Form):
+    title = StringField('Title', validators=[DataRequired()])
+    post = TextAreaField('Text', validators=[DataRequired(), Length(min=10, max=158)])
+    image = FileField('Image', validators=[
         FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
     ])
     link = StringField('Link', validators=[DataRequired()])
@@ -23,7 +32,7 @@ class SearchForm(Form):
     search_text = StringField('text', validators=[DataRequired()])
 
 
-class addMyComment(Form):
-    postId = StringField('postId', validators=[DataRequired()])
-    comment = TextAreaField('Text', validators=[DataRequired(), Length(min=1, max=256)])
-    userId = StringField('userId', validators=[DataRequired()])
+class RegistrationForm(Form):
+    full_name = StringField('Full name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password')
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 import uuid
 from datetime import datetime
-from queue import Empty
 from time import time
 from bson.objectid import ObjectId
-from flask_login import current_user
+from pymongo.errors import DuplicateKeyError
+
+from app.user import User
 
 from flask import (
     render_template,
@@ -16,32 +17,21 @@ from flask_login import login_required, current_user
 
 from app import mongo
 from app.main import main
-from app.main.forms import AddPostForm
-from app.main.forms import addComment
-from app.main.forms import SearchForm
+from app.main.forms import AddPostForm, addComment, SearchForm, RegistrationForm, EditPostForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST':
-        request.form[''];
-        flash('comment add successfully', category='success')
+    # if request.method == 'POST':
+    #     request.form[''];
+    #     flash('comment add successfully', category='success')
     post = mongo.db.posts.find()
     userbookmarks = mongo.db.users.find_one({"_id": current_user.username}, {"_id": 0, "bookmarks": 1})
     if 'bookmarks' in userbookmarks:
         userbookmarks = userbookmarks['bookmarks']
     return render_template('index.html', post=post, userbookmarks=userbookmarks)
 
-    # if userbookmarks:
-    #     ['bookmarks']
-    # else:
-    #     return render_template('index.html', post=post, userbookmarks=[])
-
-
-# @main.add_comment('/addcommenct' ,methods=['GET', 'POST'] )
-# def add_comment():
-#     return render_template('index.html')
 
 @main.route('/new_post', methods=['GET', 'POST'])
 @login_required
@@ -63,7 +53,7 @@ def new_post():
                                'tags': form.tags.data.split(','),
                                'username': current_user.username
                                })
-        request.files['image'].save("../mongodb_project/app" + file_path)
+        request.files['image'].save("app" + file_path)
         flash('Post added successfully!', category='success')
         return redirect(url_for('main.new_post'))
     return render_template('add_post.html', form=form)
@@ -74,46 +64,59 @@ def new_post():
 def editpost():
     postid = request.args.get('postid')
 
+    try:
+        post_default_values = mongo.db.posts.find_one({"_id": ObjectId(postid)})
+    except Exception as e:
+        return 'wrong id'
+    if post_default_values == None:
+        return 'wrong id'
+
+    if current_user.username != post_default_values['username']:
+        return 'you do not have access to edit this post'
+
     if request.method == 'GET':
-        add_post_form = AddPostForm()
-        try:
-            post_default_values = mongo.db.posts.find_one({"_id": ObjectId(postid)})
-        except Exception as e:
-            return 'wrong id'
-        if post_default_values == None:
-            return 'wrong id'
+        edit_post_form = EditPostForm()
 
-        if current_user.username != post_default_values['username']:
-            return 'you do not have access to edit this post'
-
-        add_post_form.title.data = post_default_values['title']
-        add_post_form.post.data = post_default_values['post_body']
-        add_post_form.link.data = post_default_values['link']
+        edit_post_form.title.data = post_default_values['title']
+        edit_post_form.post.data = post_default_values['post_body']
+        edit_post_form.link.data = post_default_values['link']
         tags = ''
         for t in post_default_values['tags']:
             tags += t + ','
         tags = tags[:-1]
-        add_post_form.tags.data = tags
+        edit_post_form.tags.data = tags
 
-        return render_template('add_post.html', title='Add', form=add_post_form)
+        return render_template('add_post.html', title='Add', form=edit_post_form)
 
-    form = AddPostForm()
+    form = EditPostForm()
     published_on = datetime.fromtimestamp(time()).strftime('%d-%m-%Y in %H:%M')
-    file_type = request.files['image'].filename.split('.')[-1]
     if form.validate_on_submit():
-        file_path = "/static/img/" + str(uuid.uuid1()) + '.' + file_type
-        mongo.db.posts.save(
-            {'_id': ObjectId(postid),
-             'title': form.title.data,
-             'post_body': form.post.data,
-             'published_on': published_on,
-             'image': file_path,
-             'image_file_name': request.files['image'].filename,
-             'link': form.link.data,
-             'tags': form.tags.data.split(','),
-             'username': current_user.username})
+        if 'image' in request.files and request.files['image'].content_length > 0:
+            file_type = request.files['image'].filename.split('.')[-1]
+            file_path = "/static/img/" + str(uuid.uuid1()) + '.' + file_type
+            request.files['image'].save("app" + file_path)
+            mongo.db.posts.update(
+                {'_id': ObjectId(postid),
+                 'title': form.title.data,
+                 'post_body': form.post.data,
+                 'published_on': published_on,
+                 'image': file_path,
+                 'image_file_name': request.files['image'].filename,
+                 'link': form.link.data,
+                 'tags': form.tags.data.split(','),
+                 'username': current_user.username})
+        else:
+            mongo.db.posts.update(
+                {"_id": ObjectId(postid)},
+                {"$set": {
+                    'title': form.title.data,
+                    'post_body': form.post.data,
+                    'published_on': published_on,
+                    'link': form.link.data,
+                    'tags': form.tags.data.split(',')}
+                }
+            )
 
-        request.files['image'].save("../mongodb_project/app" + file_path)
         flash('Post edited successfully!', category='success')
         return redirect(url_for('main.profile'))
     return render_template('add_post.html', form=form)
@@ -125,10 +128,13 @@ def delete_post():
     postid = request.args.get('postid')
     if current_user.username == mongo.db.posts.find_one({'_id': ObjectId(postid)})['username']:
         mongo.db.posts.delete_one({'_id': ObjectId(postid)})
+        for user in mongo.db.users.find({}, {"_id": 1}):
+            mongo.db.users.update({"_id": user['_id']}, {"$pull": {"bookmarks": postid}})
     return redirect(url_for('main.profile'))
 
 
 @main.route('/search', methods=['GET', 'POST'])
+@login_required
 def search():
     if request.method == 'GET':
         return render_template('search.html', title='Search', form=SearchForm())
@@ -140,8 +146,8 @@ def search():
             {"$or": [{"title": {"$regex": searched_text}}, {"post_body": {"$regex": searched_text}}]})
 
         users = mongo.db.users.find(
-            {"$or": [{"_id": {"$regex": searched_text}}, {"full_name": {"$regex": searched_text}}]} ,
-            {"_id" : 1, "full_name" : 1 }
+            {"$or": [{"_id": {"$regex": searched_text}}, {"full_name": {"$regex": searched_text}}]},
+            {"_id": 1, "full_name": 1}
         )
 
         if posts.count() == 0:
@@ -149,7 +155,7 @@ def search():
         if users.count() == 0:
             flash('no user found', category='warning')
 
-        return render_template('search.html', title='Search', form=form, post=posts , users = users)
+        return render_template('search.html', title='Search', form=form, post=posts, users=users)
 
     return render_template('search.html', form=form)
 
@@ -157,10 +163,13 @@ def search():
 @main.route('/mybookmarks')
 @login_required
 def mybookmarks():
-    bookmark_ids = mongo.db.users.find_one({"_id": current_user.username})['bookmarks']
-    bookmark_posts = mongo.db.posts.find({"_id": {"$in": bookmark_ids}})
+    userbookmarks = mongo.db.users.find_one({"_id": current_user.username}, {"_id": 0, "bookmarks": 1})
+    bookmark_posts = []
+    if 'bookmarks' in userbookmarks:
+        userbookmarks = userbookmarks['bookmarks']
+        bookmark_posts = mongo.db.posts.find({"_id": {"$in": [ObjectId(b_id) for b_id in userbookmarks]}})
 
-    return render_template('index.html', post=bookmark_posts)
+    return render_template('index.html', post=bookmark_posts, userbookmarks=userbookmarks)
 
 
 @main.route('/profile')
@@ -169,17 +178,79 @@ def profile():
     if username == None and current_user.is_authenticated:
         username = current_user.username
 
+    post = mongo.db.posts.find({"username": username})
     can_edit = (current_user.is_authenticated and username == current_user.username)
 
     user_detail = mongo.db.users.find_one({"_id": username})
     if user_detail == None:
         return 'wrong username'
 
-    post = mongo.db.posts.find({"username": username})
-    return render_template('profile.html', post=post, user_detail=user_detail, can_edit=can_edit)
+    if 'bookmarks' in user_detail:
+        num_bookmarks = len(user_detail['bookmarks'])
+    else:
+        num_bookmarks = 0
+
+    query = list(mongo.db.posts.aggregate(
+        [{"$match": {"username": username, "like": {"$exists": True}}}, {"$project": {"num_likes": {"$size": "$like"}}},
+         {"$group": {"_id": None, "count": {"$sum": "$num_likes"}}}]))
+
+    num_posts_likes = 0
+    if query != None and len(query) > 0 and 'count' in query:
+        num_posts_likes = query[0]['count']
+
+    return render_template('profile.html', post=post, user_detail=user_detail, can_edit=can_edit,
+                           num_bookmarks=num_bookmarks, num_posts_likes=num_posts_likes)
+
+
+@main.route('/editprofile', methods=['GET', 'POST'])
+@login_required
+def editprofile():
+    username = current_user.username
+
+    if request.method == 'GET':
+        user_detail = mongo.db.users.find_one({"_id": username}, {"email": 1, "full_name": 1, "_id": 0})
+
+        registrationForm = RegistrationForm()
+        registrationForm.full_name.data = user_detail['full_name']
+        registrationForm.email.data = user_detail['email']
+
+        return render_template('editprofile.html', title='Registration',
+                               form=registrationForm)
+
+    form = RegistrationForm()
+    full_name = form.full_name.data
+    email = form.email.data
+    password = form.password.data
+
+    if form.validate_on_submit():
+        try:
+            # > db.users.update({username: "smith"}, {$set: {country: "Canada"}})
+            if password == None or len(password) == 0:
+                mongo.db.users.update(
+                    {"_id": current_user.username},
+                    {"$set": {
+                        "full_name": full_name,
+                        "email": email
+                    }})
+
+            else:
+                mongo.db.users.update(
+                    {"_id": current_user.username},
+                    {"$set": {
+                        "full_name": full_name,
+                        "email": email,
+                        "password": User.generate_hash(password)
+                    }})
+
+            flash('Successfully updated an account!', category='success')
+            return redirect(url_for("main.profile"))
+        except DuplicateKeyError:
+            flash('Username already exist', category='error')
+    return render_template('editprofile.html', form=form)
 
 
 @main.route('/showpostandcomment', methods=['GET', 'POST'])
+@login_required
 def showpostandcomment():
     s = request.args.get('myid')
     post = mongo.db.posts.find({"_id": ObjectId(s)})
@@ -196,6 +267,7 @@ def showpostandcomment():
 
 
 @main.route('/likepost', methods=['GET', 'POST'])
+@login_required
 def likepost():
     s = request.args.get('myid')
     countLike = mongo.db.posts.find({"like": current_user.username, "_id": ObjectId(s)}).count()
